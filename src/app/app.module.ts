1345212e4231d1c0878c1467703f1a39f215bb12
hellend08 <sccp.hvasquez@gmail.com>
2021-02-19 08:19:18 -0500

    Estructura báscia login y recuperar pass

---
+++ src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatMenuModule} from '@angular/material/menu';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import { LoginComponent } from './login/login.component';
+import { RecoverPasswordComponent } from './recover-password/recover-password.component';
+
+
+const routes: Routes = [
+  { path: '', component: LoginComponent, pathMatch: 'full' },
+  { path: 'login', component:  LoginComponent },
+  { path: 'recover-pass', component:  RecoverPasswordComponent },
+
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RecoverPasswordComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatGridListModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
