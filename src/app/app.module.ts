e4940baf03aea1d8d4934f95450294d7a0d99880
hellend08 <sccp.hvasquez@gmail.com>
2021-02-20 00:20:22 -0500

    probando error de style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatMenuModule } from '@angular/material/menu';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
 import { LoginComponent } from './login/login.component';
 import { RecoverPasswordComponent } from './recover-password/recover-password.component';
 import { RecaptchaModule } from "ng-recaptcha";
+// import { NgxCaptchaModule } from 'ngx-captcha';
 
 
 const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'recover-pass', component: RecoverPasswordComponent },
+  { path: 'login', component:  LoginComponent },
+  { path: 'recover-pass', component:  RecoverPasswordComponent },
 
 ];
 
@@ -32,7 +33,7 @@ const routes: Routes = [
     AppComponent,
     LoginComponent,
     RecoverPasswordComponent,
-
+   
   ],
   imports: [
     BrowserModule,
@@ -47,7 +48,7 @@ const routes: Routes = [
     MatGridListModule,
     MatIconModule,
     RecaptchaModule,
-
+    // NgxCaptchaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
