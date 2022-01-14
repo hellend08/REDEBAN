12dac7f6a936ca2dd7552cb6ee8a289a5ec83a33
Ken Ramirez <kenramirezp@gmail.com>
2022-01-14 07:20:09 -0500

    chart cantidad

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
@@ -12,20 +11,87 @@ export class ChartsComponent implements OnInit {
 
   public barChartOptions = {
     scaleShowVerticalLines: false,
-    responsive: true
+    responsive: true,
   };
 
-  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels = [
+    '8 ene.',
+    '9 ene.',
+    '10 ene.',
+    '11 ene.',
+    '12 ene.',
+    '13 ene.',
+    '14 ene.',
+    '15 ene.',
+    '16 ene.',
+    '17 ene.',
+    '18 ene.',
+    '19 ene.',
+    '20 ene.',
+    '21 ene.',
+    '22 ene.',
+    '23 ene.',
+    '24 ene.',
+    '25 ene.',
+    '26 ene.',
+    '27 ene.',
+    '28 ene.',
+    '29 ene.',
+    '30 ene.',
+    '31 ene.',
+    '1 feb.',
+    '2 feb.',
+    '3 feb.',
+    '4 feb.',
+    '5 feb.',
+    '6 feb.',
+  ];
 
   public barChartType = 'bar';
 
-  public barChartLegend = true;
+  public barChartLegend = false;
   
   public barChartData = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+    {
+      label: 'Cantidad',
+      data: [
+        480,
+        570,
+        380,
+        480,
+        260,
+        480,
+        570,
+        380,
+        480,
+        260,
+        480,
+        570,
+        380,
+        480,
+        260,
+        480,
+        570,
+        380,
+        480,
+        260,
+        480,
+        570,
+        380,
+        480,
+        260,
+        480,
+        570,
+        380,
+        480,
+        260,
+      ],
+      backgroundColor: ['#00339F', '#86AAF6'],
+      hoverBackgroundColor: ['#2855B2', '#CADBFF'],
+      borderColor: ['#00339F', '#86AAF6']
+    },
   ];
-  
+
   ngOnInit(): void {
   }
 
